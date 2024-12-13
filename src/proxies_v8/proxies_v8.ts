fca695da995ee2277585517f1db378f71eb48120
pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>
2024-12-13 15:12:34 +0530

    updated endpoint

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -1134,7 +1134,7 @@ proxiesV8.use('/community/v1/*',
   proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
 
-proxiesV8.use('/v1/recommendations/courses',
+proxiesV8.use('/recommendations/v1/courses',
   // tslint:disable-next-line: max-line-length
-  proxyCreatorSunbirdSearch(express.Router(), `${CONSTANTS.KONG_API_BASE}/v1/recommendations/courses`)
+  proxyCreatorSunbirdSearch(express.Router(), `${CONSTANTS.KONG_API_BASE}/recommendations/v1/courses`)
 )
